07519564081cdbcc9d27960b1772ece80067a6e3
Justin Mercado <mjustin@uninorte.edu.co>
2025-04-06 13:14:29 -0500

    Se agrega el proyecto

---
+++ src/core/Justin.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author RYZEN
+ */
+public class Justin {
+    private double altura;
+    private String nombre;
+    private double peso;
+
+    public Justin(float altura, String nombre, float peso) {
+        this.altura = altura;
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+    
+    
+}
