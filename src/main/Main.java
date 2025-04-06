360db60560e27b31eb21ca01ca2fe663e0ae4bfa
jxmerc10 <mjustin@uninorte.edu.co>
2025-04-06 16:37:37 -0500

    cambiando

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,5 +15,7 @@ public class Main {
     Justin justin = new Justin(178, "Daniel", 55);
     Justin justin2 = new Justin(180, "Jose", 60);
     Justin justin3 = new Justin(162, "Maria", 40);
+    Justin justin4 = new Justin(189, "Jesus", 50);
+    
        
 }
